9271e981cf5fb60444804f76e9602b6909a95589
nucklass <60252281+nucklass@users.noreply.github.com>
2021-06-16 21:39:08 -0700

    change that guy2

--- bnn/bnn.js
+++ bnn/bnn.js
@@ -8,6 +8,6 @@ let mod_weights = []
 let mod_biases = []
 var i;
 for(i=0; i=weightsandbiases.length;i++){
-    if (i % 2 == 0) biases[(i/2)] = weightsandbiases[i]
-    else weights[(i/2 + .5)] = weightsandbiases[i]
+    if (i % 2 == 0) mod_biases[(i/2)] = weightsandbiases[i]
+    else mod_weights[(i/2 + .5)] = weightsandbiases[i]
 }

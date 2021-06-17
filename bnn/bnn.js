b4d0fdb22a76c177f4dd05a320e423c3c66419b2
nucklass <60252281+nucklass@users.noreply.github.com>
2021-06-16 21:38:36 -0700

    change that guy

--- bnn/bnn.js
+++ bnn/bnn.js
@@ -2,4 +2,12 @@ let weightsandbiases = []
 
 flux.fetchVarWeights("./bnn/anime_weights.bson").then((function (ws) {
     return weightsandbiases = ws;
-  }))
+  }));
+
+let mod_weights = []
+let mod_biases = []
+var i;
+for(i=0; i=weightsandbiases.length;i++){
+    if (i % 2 == 0) biases[(i/2)] = weightsandbiases[i]
+    else weights[(i/2 + .5)] = weightsandbiases[i]
+}
